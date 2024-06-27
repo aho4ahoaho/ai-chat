15090967b26485b8da29e8806328b8b801493217
aho4ahoaho <aho4ahoaho@outlook.jp>
2024-06-27 11:49:10 +0900

    不要なファイルの削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Main } from './main';
-import './index.css'
 
 
 const container = document.querySelector('#root');
-const root = createRoot(container);
+if (container) {
+    const root = createRoot(container);
 
-root.render(<Main />);
+    root.render(<Main />);
+} else {
+    throw new Error('Root element not found');
+}
